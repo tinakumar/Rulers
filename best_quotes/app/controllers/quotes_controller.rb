2807b18993537880b89b933bf470b43124bb1e3b
Tina Kumar <tinakumar07@gmail.com>
2013-11-05 23:38:20 -0800

    completed chapter 2. added controller.

---
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -0,0 +1,9 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing good or bad " +
+      "but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
+  end
+
+
+end
