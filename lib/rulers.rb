69f743485ae61d53797a8bc420b89d43e6882252
Tina Kumar <tinakumar07@gmail.com>
2013-11-05 22:36:21 -0800

    finished chapter 1. added tests.

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,7 +6,7 @@ module Rulers
     def call(env)
       `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
-        ['Hello from Ruby on Rulers!']]
+        ["Hello from Ruby on Rulers!"]]
     end
   end
 end
