d394a78c5fe766bd7fa473056ff08082c132903a
Tina Kumar <tinakumar07@gmail.com>
2013-11-11 18:20:06 -0800

    chapter 6

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -13,6 +13,12 @@ class QuotesController < Rulers::Controller
     render :index, :quotes => quotes
   end
 
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
+
   def new_quote
     attrs = {
       "submitter" => "web user",
@@ -31,11 +37,5 @@ class QuotesController < Rulers::Controller
     }
     n = FileModel.update attrs
     render :index, :quotes => quotes
-  end
-
-  def show
-    quote = FileModel.find(params["id"])
-    ua = request.user_agent
-    render_response :quote, :obj => quote, :ua => ua
   end
 end
--- rulers/lib/rulers/controller.rb
+++ rulers/lib/rulers/controller.rb
@@ -26,5 +26,27 @@ module Rulers
       klass = klass.to_s.gsub /Controller$/, ""
       Rulers.to_underscore klass
     end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
   end
 end
